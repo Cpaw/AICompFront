c2b8876387549acc3285025d897bc653c4f78895
palloc <palloc.mknet10@gmail.com>
2018-03-09 00:21:15 +0900

    add some files

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <nav>
+      <router-link tag="li" :to="{ path: '/'}">
+        <a class="navigate">Home</a>
+      </router-link>
+      <router-link tag="li" :to="{ path: '/'}">
+        <a class="navigate">Home</a>
+      </router-link>
+      <router-link tag="li" :to="{ path: '/'}">
+        <a class="navigate">Home</a>
+      </router-link>
+      <router-link tag="li" :to="{ path: '/'}">
+        <a class="navigate">Home</a>
+      </router-link>
+
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
