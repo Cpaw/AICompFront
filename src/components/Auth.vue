e1aff86867195944f0d41490e36cf02bf94e3aaa
palloc <palloc.mknet10@gmail.com>
2018-03-12 20:24:34 +0900

    add auth

---
+++ src/components/Auth.vue
@@ -0,0 +1,46 @@
+<template>
+  <article>
+    <section class="signin">
+      <h2>Signin</h2>
+      <form>
+        <div class="field">
+          <p>Name</p>
+          <input class="name" type="text" name="name" placeholder="name" v-model="signin.name">
+        </div>
+        <div class="field">
+          <p>Pass</p>
+          <input class="pass" type="password" name="password" placeholder="password" v-model="signin.password">
+        </div>
+      </form>
+    </section>
+  </article>
+</template>
+<script>
+import {HTTP} from './Header'
+
+export default {
+  data () {
+    return {
+      signin: {
+        name: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    Signin: function () {
+      HTTP.post('signin',
+        this.$data.signin,
+        {
+          headers: {'Content-Type': 'application/json'},
+          withCredentials: true
+        })
+        .then(response => {
+          this.$router.push('/problems')
+        })
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import Problems from '@/components/Problems'
 import Ranking from '@/components/Ranking'
-import Signout from '@/components/Signout'
+import Auth from '@/components/Auth'
 
 Vue.use(Router)
 
@@ -27,7 +27,7 @@ export default new Router({
     {
       path: '/auth',
       name: 'auth',
-      component: Signout
+      component: Auth
     }
   ]
 })
