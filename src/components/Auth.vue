9a9e8babb5e40a33ddeb91712992dbad900a06b7
palloc <palloc.mknet10@gmail.com>
2018-03-15 00:05:52 +0900

    add signup

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -2,7 +2,7 @@
   <article>
     <section class="signin">
       <h2>Signin</h2>
-      <form>
+      <form v-on:submit.prevent="Signin">
         <div class="field">
           <p>Name</p>
           <input class="name" type="text" name="name" placeholder="name" v-model="signin.name">
@@ -11,7 +11,28 @@
           <p>Pass</p>
           <input class="pass" type="password" name="password" placeholder="password" v-model="signin.password">
         </div>
+        <div class="field">
+          <button type="submit">Submit</button>
+        </div>
       </form>
+      <div id="error_code"></div>
+    </section>
+    <section class="signup">
+      <h2>Signup</h2>
+      <form v-on:submit.prevent="Signup">
+        <div class="field">
+          <p>Name</p>
+          <input class="name" type="text" name="name" placeholder="name" v-model="signup.name">
+        </div>
+        <div class="field">
+          <p>Pass</p>
+          <input class="pass" type="password" name="password" placeholder="password" v-model="signup.password">
+        </div>
+        <div class="field">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <div id="error_code"></div>
     </section>
   </article>
 </template>
@@ -22,6 +43,10 @@ export default {
   data () {
     return {
       signin: {
+        name: '',
+        password: ''
+      },
+      signup: {
         name: '',
         password: ''
       }
@@ -38,9 +63,40 @@ export default {
         .then(response => {
           this.$router.push('/problems')
         })
+        .catch(e => {
+          document.getElementById('error_code').innerHTML = '<p>Invalid input data.</p>'
+        })
+    },
+    Signup: function () {
+      HTTP.post('signup',
+        this.$data.signup,
+        {
+          headers: {'Content-Type': 'application/json'},
+          withCredentials: true
+        })
+        .then(response => {
+          this.$router.push('/')
+        })
+        .catch(e => {
+          document.getElementById('error_code').innerHTML = '<p>Invalid input data.</p>'
+        })
     }
   }
 }
 </script>
 <style scoped>
+.signin {
+  background: white;
+  width: 30vw;
+  margin: 10vh auto 0 auto;
+  border: solid 3.15px #6699cc;
+  border-radius: 10px 10px;
+}
+.signup {
+  background: white;
+  width: 30vw;
+  margin: 10vh auto 0 auto;
+  border: solid 3.15px #6699cc;
+  border-radius: 10px 10px;
+}
 </style>
